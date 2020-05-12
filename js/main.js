423a0aba7cec5cbdd997eab1f167a0161587f3f6
anderood <anderood@hotmail.com>
2020-05-12 06:56:12 -0300

    Criada a Função para Limpar os Campos Criada a Função para Criar a Estrutura do XML Criada a Função para Validação dos Campos

--- js/main.js
+++ js/main.js
@@ -1,44 +1,88 @@
 
+var inputTitulo = document.querySelector('#inptTitle');
+var inputLink = document.querySelector("#inptLink");
+var inputFanart = document.querySelector("#inptFanart");
+var inputThumb = document.querySelector("#inptThumb");
+var inputGenero = document.querySelector("#inptGenere");
+var inputData = document.querySelector("#inptDatadeLancamento");
 function criaXmlKodi(){
-    var inputTitulo = document.querySelector("inptTitle");
-    var inputLink = document.querySelector("inptLink");
-    var inputFanart = document.querySelector("inptFanart");
-    var inputThumb = document.querySelector("inptThumb");
-    var inputGenero = document.querySelector("inptGenere");
-    var inputData = document.querySelector("inptDatadeLancamento");
-
+    
     var doc = document.implementation.createDocument("<?xml version='1.0'?>", "", null);
 
     var filmesElem = doc.createElement('filmes');
 
     var tituloElem = doc.createElement('title');
-    tituloElem.textContent = 'oi'
+    var txtTitle = doc.createTextNode(inputTitulo.value)
+    tituloElem.appendChild(txtTitle);
 
 
     var linkElem = doc.createElement('link');
+    var txtLink = doc.createTextNode(inputLink.value)
+    linkElem.appendChild(txtLink);
 
     var fanartElem = doc.createElement('fanart');
-
+    var txtFanart = doc.createTextNode(inputFanart.value)
+    fanartElem.appendChild(txtFanart);
 
     var thumbElem = doc.createElement('thumbnail');
-
+    var txtGenre = doc.createTextNode(inputThumb.value)
+    thumbElem.appendChild(txtGenre);
 
     var genreElem = doc.createElement('genre');
-
+    var txtGenre = doc.createTextNode(inputGenero.value)
+    genreElem.appendChild(txtGenre);
 
     var dataElem = doc.createElement('data');
-
-
-
-    filmesElem.append(tituloElem);
-    filmesElem.append(linkElem);
-    filmesElem.append(fanartElem);
-    filmesElem.append(thumbElem);
-    filmesElem.append(genreElem);
-    filmesElem.append(dataElem);
-
-    console.log(filmesElem);
+    var txtData = doc.createTextNode(inputData.value)
+    dataElem.appendChild(txtData);
     
+    filmesElem.appendChild(tituloElem);
+    filmesElem.appendChild(linkElem);
+    filmesElem.appendChild(fanartElem);
+    filmesElem.appendChild(thumbElem);
+    filmesElem.appendChild(genreElem);
+    filmesElem.appendChild(dataElem);
+    
+    console.log(filmesElem);
+}
+
+function validaCampos(){
+    if(inputTitulo.value == ""){
+        alert("preencha o campo")
+    }
+
+    if(inputLink.value == ""){
+        alert("preencha o campo")
+    }
+
+    if(inputFanart.value == ""){
+        alert("preencha o campo")
+    }
+
+    if(inputThumb.value == ""){
+        alert("preencha o campo")
+    }
+
+    if(inputGenero.value == ""){
+        alert("preencha o campo")
+    }
+
+    if(inputData.value == ""){
+        alert("preencha o campo")
+    }
 
 }
 
+
+function retiraEspacos(){
+
+}
+
+function limpaCampos(){
+    inputTitulo.textContent = ""
+    inputLink.textContent = ""
+    inputFanart.textContent = ""
+    inputThumb.textContent = ""
+    inputGenero.textContent = ""
+    inputData.textContent = ""
+}

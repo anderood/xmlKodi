79a447b11e3a509f9e1c75513b1842a874901100
anderood <anderoodsantos@gmail.com>
2020-05-17 03:51:25 -0300

    Adicionado Template do XML Adicionado os Valores dentro do Template Criado a Função para Salvar os arquivos em XML

--- js/main.js
+++ js/main.js
@@ -1,67 +1,82 @@
 
+var inputTituloArquivo = document.querySelector('#inptTitleArquivo');
 var inputTitulo = document.querySelector('#inptTitle');
 var inputLink = document.querySelector("#inptLink");
 var inputFanart = document.querySelector("#inptFanart");
 var inputThumb = document.querySelector("#inptThumb");
 var inputGenero = document.querySelector("#inptGenere");
 var inputData = document.querySelector("#inptDatadeLancamento");
-function criaXmlKodi(){
+// function criaXmlKodi(){
     
-    var doc = document.implementation.createDocument("<?xml version='1.0'?>", "", null);
+//     var doc = document.implementation.createDocument(null, "biblioteca", null);
 
-    var filmesElem = doc.createElement('filmes');
+//     var filmesElem = doc.createElement('filmes');
 
-    var tituloElem = doc.createElement('title');
-    var txtTitle = doc.createTextNode(inputTitulo.value)
-    tituloElem.appendChild(txtTitle);
+//     var tituloElem = doc.createElement('title');
+//     var txtTitle = doc.createTextNode(inputTitulo.value)
+//     tituloElem.appendChild(txtTitle);
 
 
-    var linkElem = doc.createElement('link');
-    var txtLink = doc.createTextNode(inputLink.value)
-    linkElem.appendChild(txtLink);
+//     var linkElem = doc.createElement('link');
+//     var txtLink = doc.createTextNode(inputLink.value)
+//     linkElem.appendChild(txtLink);
 
-    var fanartElem = doc.createElement('fanart');
-    var txtFanart = doc.createTextNode(inputFanart.value)
-    fanartElem.appendChild(txtFanart);
+//     var fanartElem = doc.createElement('fanart');
+//     var txtFanart = doc.createTextNode(inputFanart.value)
+//     fanartElem.appendChild(txtFanart);
 
-    var thumbElem = doc.createElement('thumbnail');
-    var txtGenre = doc.createTextNode(inputThumb.value)
-    thumbElem.appendChild(txtGenre);
+//     var thumbElem = doc.createElement('thumbnail');
+//     var txtGenre = doc.createTextNode(inputThumb.value)
+//     thumbElem.appendChild(txtGenre);
 
-    var genreElem = doc.createElement('genre');
-    var txtGenre = doc.createTextNode(inputGenero.value)
-    genreElem.appendChild(txtGenre);
+//     var genreElem = doc.createElement('genre');
+//     var txtGenre = doc.createTextNode(inputGenero.value)
+//     genreElem.appendChild(txtGenre);
 
-    var dataElem = doc.createElement('data');
-    var txtData = doc.createTextNode(inputData.value)
-    dataElem.appendChild(txtData);
+//     var dataElem = doc.createElement('data');
+//     var txtData = doc.createTextNode(inputData.value)
+//     dataElem.appendChild(txtData);
     
-    filmesElem.appendChild(tituloElem);
-    filmesElem.appendChild(linkElem);
-    filmesElem.appendChild(fanartElem);
-    filmesElem.appendChild(thumbElem);
-    filmesElem.appendChild(genreElem);
-    filmesElem.appendChild(dataElem);
+//     filmesElem.appendChild(tituloElem);
+//     filmesElem.appendChild(linkElem);
+//     filmesElem.appendChild(fanartElem);
+//     filmesElem.appendChild(thumbElem);
+//     filmesElem.appendChild(genreElem);
+//     filmesElem.appendChild(dataElem);
+
+//     doc.documentElement.appendChild(filmesElem);
     
-    return filmesElem;
+//     return doc;
     
-    //limpaCampos();
+//     //limpaCampos();
     
-}
+// }
 
 function salvarXml(){
-    
+    var criarXml = document.getElementById('Salva');
 
-    let estrutura = criaXmlKodi();
-    console.log(estrutura);
-    
-    var inputTitulo = document.querySelector('#inptTitle');
-    console.log("AQ " + inputTitulo.value);
-    
-    let blob = new Blob([inputTitulo], { type: "text/plain;charset=utf-8"});
-    console.log(blob)
-    saveAs(blob, inputTitulo.value + ".txt");
+    criarXml.addEventListener('click', function(event){
+        event.preventDefault();
 
+        var templateXML = `<?xml version="1.0" encoding="UTF-8"?>
+<BIBLIOTECA>
+  <FILMES>
+    <TITLE>${inputTitulo.value}</TITLE>
+    <LINK>${inputLink.value}</LINK>
+    <FANART>${inputFanart.value}</FANART>
+    <THUMBNAIL>${inputThumb.value}</THUMBNAIL>
+    <GENRE>${inputGenero.value}</GENRE>
+    <DATA>${inputData.value}</DATA>
+  </FILMES>
+</BIBLIOTECA>`
+        
+        var blob = new Blob([templateXML], {type: "text/xml"});
+
+        //saveAs(blob, "titulo.xml");
+        
+        saveAs(blob, inputTituloArquivo.value,"text/plain;charset=utf-8");
+    });
+    
 }
 
 function validaCampos(){
@@ -96,12 +111,3 @@ function retiraEspacos(){
 
 }
 
-function limpaCampos(){
-    
-    inputTitulo.value = ""
-    inputLink.value = ""
-    inputFanart.value = ""
-    inputThumb.value = ""
-    inputGenero.value = ""
-    inputData.value = ""
-}

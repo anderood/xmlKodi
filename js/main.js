c740aa72a002c44447f150e1dceabb068a9e4e84
anderood <anderood@hotmail.com>
2020-06-19 08:47:00 -0300

    Alguns Ajustes

--- js/main.js
+++ js/main.js
@@ -8,39 +8,28 @@ var inputThumb = document.querySelector("#inptThumb");
 var inputGenero = document.querySelector("#inptGenere");
 var inputData = document.querySelector("#inptDatadeLancamento");
 
-var salvarXml = document.addEventListener('click', function(event){
+var salvarXML = document.addEventListener('click', function(event){
 
     event.preventDefault();
-    
-    if(inputTituloArquivo.value || inputTitulo.value == ""){
-        alert("Preecha os Campos")
+
+    var itensXML = {
+        id: Date.now(),
+        nomeArquivo: inputTituloArquivo.value,
+        itens: [
+            inputTitulo.value, 
+            inputLink.value, 
+            inputFanart.value, 
+            inputThumb.value,  
+            inputGenero.value, 
+            inputData.value
+        ]
     }
-    
-
-
-    var itensXML = [
-        id = Date.now(),
-        NomeDoArquivo = inputTituloArquivo.value,
-        link = inputLink.value,
-        fanart = inputFanart.value,
-        thumb = inputThumb.value,
-        genero = inputGenero.value, 
-        data = inputData.value
-    ];
 
     console.log(itensXML);
-    
-
-    
-
 });
 
 var limpaCampos = document.addEventListener('click', function(){
-    inputTituloArquivo.value = ""
-    inputTitulo.value = ""
-    inputLink.value = ""
-    inputFanart.value = ""
-    inputThumb.value = ""
-    inputGenero.value = ""
-    inputData.value = ""
+
+    var form = document.getElementById('form');
+    form.reset();
 });

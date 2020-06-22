369026e245dd5c690e7329e299751396b56b1292
anderood <anderood@hotmail.com>
2020-06-22 06:44:30 -0300

    Elaborado a Logica do Novo Item

--- js/main.js
+++ js/main.js
@@ -7,12 +7,14 @@ var inputFanart = document.querySelector("#inptFanart");
 var inputThumb = document.querySelector("#inptThumb");
 var inputGenero = document.querySelector("#inptGenere");
 var inputData = document.querySelector("#inptDatadeLancamento");
+var itensXML = {};
+var itens = [];
 
-var salvarXML = document.addEventListener('click', function(event){
+var salvaXML = document.querySelector('#salvarXML');
+salvaXML.addEventListener('click', function(event){
 
     event.preventDefault();
-
-    var itensXML = {
+    itensXML = {
         id: Date.now(),
         nomeArquivo: inputTituloArquivo.value,
         itens: [
@@ -24,11 +26,49 @@ var salvarXML = document.addEventListener('click', function(event){
             inputData.value
         ]
     }
+    console.log("Estou em SalvarXML" + itensXML.id);
+    
+    var templateXML = `<?xml version="1.0" encoding="UTF-8"?>
+<BIBLIOTECA>
+  <FILMES>
+    <TITLE>${itensXML.itens[0]}</TITLE>
+    <LINK>${itensXML.itens[1]}</LINK>
+    <FANART>${itensXML.itens[2]}</FANART>
+    <THUMBNAIL>${itensXML.itens[3]}</THUMBNAIL>
+    <GENRE>${itensXML.itens[4]}</GENRE>
+    <DATA>${itensXML.itens[5]}</DATA>
+  </FILMES>
+</BIBLIOTECA>`
 
-    console.log(itensXML);
+
+    console.log(templateXML);
+
+    var form = document.getElementById('form');
+    form.reset();
+    // var blob = new Blob([templateXML], {type: "text/xml"});
+    // saveAs(blob, itensXML.nomeArquivo,"text/plain;charset=utf-8");
 });
 
-var limpaCampos = document.addEventListener('click', function(){
+var novoItem = document.querySelector('#novoItem');
+novoItem.addEventListener('click', function(event){
+    event.preventDefault();
+    
+    var elementoXML = itensXML;
+    var idx = itens.indexOf(elementoXML);
+
+    for(var i = 0; i < 1; i++){
+        
+        if(idx == -1){
+            itens.push(itensXML);
+        }else{
+            alert('Dado Repetido')    
+        }
+    }
+    console.log(itens);
+});
+
+var limpaCampos = document.querySelector('#limpaCampos');
+limpaCampos.addEventListener('click', function(){
 
     var form = document.getElementById('form');
     form.reset();
